4b826e54df2d804cf3041af3d7f0b30a934a9a16
JoaolimaDev <ozymandiasphp@gmail.com>
2023-10-17 19:35:17 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+
+        String[] agx = {"asdad", "asdasdads"};
+
+        String[] argx_02 = new String[10];
+
+        int[] arr = new int[4];
+
+        for (int index = 0; index < arr.length; index++) {
+            
+            arr[index] = index;
+
+            System.out.println(arr[index]);
+
+
+        }
+        
+    }
+}
